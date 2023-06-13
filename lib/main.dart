3c7ea548e6932c0debc6cb2a22ad879ce8bdbb49
Pranshu Garg <134578489+PranshuPeopleLink@users.noreply.github.com>
2023-06-13 17:49:48 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:getx_mvvm/res/routes/routes.dart';
 import 'package:getx_mvvm/views/splash_screen.dart';
 
 void main() {
@@ -11,11 +13,15 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Center(
           child: SplashScreen(),
         ),
+        body: Center(child: SplashScreen()),
       ),
+      getPages: AppRoute.appRoute(),
     );
   }
 }
